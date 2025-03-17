fd62999a157c934e6e5a760619d8a18e2152dd37
xpc123123 <1320401599@qq.com>
2025-03-17 14:38:10 +0800

    add ex1.py to repository

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#enter a number and double it 
+#Auther:xiepengcheng
+#DSTE:17/3/25
+
+
+
+
+
+
+#enter the number
+a= input("the first number")
+#
+res = int(a)*2
+print(res)
---
+++ ex2.py
@@ -0,0 +1,5 @@
+# Write the python code which prompts the user for their name.  Output their name 
+#in all capital letters.
+#Auther:xiepengcheng
+#DSTE:17/3/2025
+print("please enter your name")
